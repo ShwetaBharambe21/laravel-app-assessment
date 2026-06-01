a3f5465f02bb538d16f8d72a1b3589d05e983383
Shweta Bharambe <shwetabharambe2196@gmail.com>
2026-06-01 10:28:16 +0530

    final commit

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,15 +4,15 @@ set -e
 
 VERSION=$1
 
-docker pull shweta779/laravel-app:$TAG
+docker pull shweta779/laravel-app:$VERSION
 
 docker stop laravel-app || true
 docker rm laravel-app || true
 
 docker run -d \
---name laravel-app \
--p 9000:9000 \
-myrepo/laravel-app:$VERSION
+  --name laravel-app \
+  -p 9000:9000 \
+  shweta779/laravel-app:$VERSION
 
 curl http://localhost/health
 
